af87fb899b51130a7c8129df67fc137d6b44d02d
Jason <chenjs@farwalker.cn>
2017-01-06 18:39:57 +0800

    init

---
+++ consumer-feign/src/main/java/cn/waka/learning/springcloud/consumer/FeignApplication.java
@@ -0,0 +1,17 @@
+package cn.waka.learning.springcloud.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+@EnableFeignClients
+public class FeignApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(FeignApplication.class, args);
+	}
+
+}
---
+++ consumer-feign/src/main/java/cn/waka/learning/springcloud/consumer/controller/ConsumerController.java
@@ -0,0 +1,21 @@
+package cn.waka.learning.springcloud.consumer.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import cn.waka.learning.springcloud.consumer.service.ComputeClient;
+
+
+
+@RestController
+public class ConsumerController {
+	@Autowired
+	ComputeClient computeClient;
+
+	@RequestMapping(value="/add", method=RequestMethod.GET)
+	public Integer add(){
+		return computeClient.add(10, 20);
+	}
+}
